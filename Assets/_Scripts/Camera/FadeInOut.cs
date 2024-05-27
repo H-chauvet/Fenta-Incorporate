ef6f7a643fae673f15310d35f23240edc7f216aa
Bubelelele <89357128+Bubelelele@users.noreply.github.com>
2024-05-27 14:49:15 +0200

    Update FadeInOut.cs

--- Assets/_Scripts/Camera/FadeInOut.cs
+++ Assets/_Scripts/Camera/FadeInOut.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 [RequireComponent(typeof(CanvasGroup))]
 public class FadeInOut : MonoBehaviour
 {
-    [SerializeField] private bool fadeIn = false;
     [SerializeField] private float fadeDuration = 2;
-    
     private CanvasGroup _canvasGroup;
     
     private void Awake() => _canvasGroup = GetComponent<CanvasGroup>();
-
-    private void Start()
-    {
-        if (fadeIn)
-            FadeIn();
-        else
-            FadeOut();
-    }
 
     public void FadeIn() => StartCoroutine(FadeCanvasGroup(_canvasGroup, 1, 0, fadeDuration));
     public void FadeOut() => StartCoroutine(FadeCanvasGroup(_canvasGroup, 0, 1, fadeDuration));
