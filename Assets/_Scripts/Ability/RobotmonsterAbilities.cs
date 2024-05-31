66e580c919c15ef34656f47a8808939a9a54b50f
Nicolas <chastinlive.fr@gmail.com>
2024-05-31 12:57:30 +0200

    Push prefab and some script modification

--- Assets/_Scripts/Ability/RobotmonsterAbilities.cs
+++ Assets/_Scripts/Ability/RobotmonsterAbilities.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class RobotmonsterAbilities : MonoBehaviour
 {
 
+    public float lightningPowerCooldown = 5f;
+    public float waterPowerCooldown = 5f;
+
+    private float lightningPowerDuration = 5f;
+    private float waterPowerDuration = 5f;
+
+    private bool lightningPowerReady = true;
+    private bool waterPowerReady = true;
+
     private GameObject parent;
     // Start is called before the first frame update
     void Start()
@@ -12,12 +21,46 @@ public class RobotmonsterAbilities : MonoBehaviour
         parent = transform.parent.gameObject;
     }
 
+    void Update()
+    {
+        if (lightningPowerReady == false)
+        {
+            lightningPowerDuration -= Time.deltaTime;
+            if (lightningPowerDuration <= 0)
+            {
+                Debug.Log("Lightning");
+                lightningPowerReady = true;
+                lightningPowerDuration = lightningPowerCooldown;
+            }
+        }
+        if (waterPowerReady == false)
+        {
+            waterPowerDuration -= Time.deltaTime;
+            if (waterPowerDuration <= 0)
+            {
+                waterPowerReady = true;
+                waterPowerDuration = waterPowerCooldown;
+            }
+        }
+    }
+
     public void LightningPower(GameObject newLocation)
     {
+        if (lightningPowerReady == false)
+        {
+            return;
+        }
+        lightningPowerReady = false;
         parent.transform.position = newLocation.transform.position;
+        
     }
     public void WaterPower()
     {
+        if (waterPowerReady == false)
+        {
+            return;
+        }
+        waterPowerReady = false;
         Debug.Log("Water");
     }
 }
