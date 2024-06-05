94a204ae7b2e79f29b4939bac581255aa55a2e8d
Henri Chauvet <henri.chauvet@epitech.eu>
2024-06-05 15:54:32 +0200

    [UPDATE] Some stuffs

--- Assets/_Scripts/Ability/Hijsi/HijsiAbilities.cs
+++ Assets/_Scripts/Ability/Hijsi/HijsiAbilities.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DashAbility : MonoBehaviour, IMonsterAbilities
+public class HijsiAbilities : MonoBehaviour, IMonsterAbilities
 {
     public float dashTime = 1.0f;
     public float dashSpeed = 10.0f;
     public float dashCooldown = 2.0f;
 
     private float currentDashTime;
-    private float currentSpeed = 1.0f;
-
     private bool isDashing = false;
     private Vector3 moveDirection;
 
@@ -22,62 +20,64 @@ public class DashAbility : MonoBehaviour, IMonsterAbilities
     void Start()
     {
         parent = transform.parent.gameObject;
-        currentDashTime = dashTime;
     }
 
     void Update()
     {
         if (isDashing)
         {
-            currentDashTime += Time.deltaTime;
-            if (currentDashTime < dashTime)
-            {
-                parent.transform.Translate(moveDirection * dashSpeed * Time.deltaTime, Space.World);
-            }
-            else
-            {
-                isDashing = false;
-                currentDashTime = dashTime;
-                moveDirection = Vector3.zero;
-                canDash = false;
-                currentCooldownTime = 0.0f;
-            }
+            HandleDash();
         }
         else
         {
-            parent.transform.Translate(moveDirection * currentSpeed * Time.deltaTime, Space.World);
-
-            if (!canDash)
-            {
-                currentCooldownTime += Time.deltaTime;
-                if (currentCooldownTime >= dashCooldown)
-                {
-                    canDash = true;
-                }
-            }
+            HandleCooldown();
         }
     }
 
     public void MainAbilityInteraction()
     {
-        if (canDash)
-        {
-            Dash();
-        }
-    }
-
-    private void Dash()
-    {
-        if (isDashing) return;
-
-        isDashing = true;
-        currentDashTime = 0.0f;
-        moveDirection = parent.transform.forward;
-        currentSpeed = dashSpeed;
+        Dash();
     }
 
     public void SecondaryAbilityInteraction()
     {
-        // Implémentez une autre capacité secondaire si nécessaire
+        // Special dash
+    }
+
+    private void Dash()
+    {
+        if (!canDash || isDashing) return;
+
+        isDashing = true;
+        currentDashTime = 0.0f;
+        moveDirection = parent.transform.forward;
+    }
+
+    private void HandleDash()
+    {
+        currentDashTime += Time.deltaTime;
+        if (currentDashTime < dashTime)
+        {
+            parent.transform.Translate(moveDirection * dashSpeed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            isDashing = false;
+            moveDirection = Vector3.zero;
+            canDash = false;
+            currentCooldownTime = 0.0f;
+        }
+    }
+
+    private void HandleCooldown()
+    {
+        if (!canDash)
+        {
+            currentCooldownTime += Time.deltaTime;
+            if (currentCooldownTime >= dashCooldown)
+            {
+                canDash = true;
+            }
+        }
     }
 }
